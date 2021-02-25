77b3db7d7c6f6d7900f36f1781c87d649977420d
Yue Da Chua <chua_yueda@hotmail.com>
2021-02-25 23:33:10 +0800

    updated gordon_growth_model

--- quantifin/equity/test/test_stock.py
+++ quantifin/equity/test/test_stock.py
@@ -1,10 +1,22 @@
 import unittest
 from unittest.mock import patch, MagicMock
+from pathlib import Path
+from json import load
 import yahoofinancials as yf
 from quantifin.equity import Stock
-from quantifin.equity.valuation import CAPM
+from quantifin.equity.valuation import *
+from quantifin.util import markets
 
 class TestCase(unittest.TestCase):
+    def setUp(self):
+        self.base_path = Path(__file__).parent
+        self.index_json = self.read_json(str(self.base_path) + "/resource/market_price.json")
+    
+
+    def read_json(self, file_path):
+        with open (file_path) as f:
+            json_dict = load(f)
+        return json_dict
 
     @patch('yahoofinancials.YahooFinancials.get_beta')
     def test_initialise_stock(self, MockGetBeta):
@@ -14,5 +26,18 @@ class TestCase(unittest.TestCase):
         self.assertTrue(MockGetBeta.called)
     
     def test_CAPM(self):
-        req_rate_CAPM = CAPM(2, 10, 1.2)
-        self.assertEqual(req_rate_CAPM, 11.6)
+        req_rate_CAPM = CAPM(0.02, 0.1, 1.2)
+        self.assertEqual(req_rate_CAPM, 0.116)
+    
+    def test_gordon_growth_model(self):
+        current_dividend = 2.0 #NEEDS IMPLEMENTATION
+        req_rate = CAPM(0.02, 0.10, 1.2)
+        print(req_rate)
+        growth_rate = 0.1 #NEEDS IMPLEMENTATION
+
+        self.assertRaises(ValueError, gordon_growth_model, -2, req_rate, growth_rate)
+        self.assertRaises(ValueError, gordon_growth_model, current_dividend, -0.116, growth_rate)
+        self.assertEqual(gordon_growth_model(current_dividend, req_rate, growth_rate), 137.5)
+        
+        
+
--- quantifin/equity/valuation/valuation.py
+++ quantifin/equity/valuation/valuation.py
@@ -3,3 +3,14 @@ from quantifin.util import markets
 
 def CAPM(risk_free: float, market_premium: float, beta: float):
     return risk_free + beta * (market_premium - risk_free)
+
+def gordon_growth_model(current_dividend: float, req_rate: float, growth_rate: float):
+    if  __any_negative_value([current_dividend, req_rate]):
+        raise ValueError("Invalid value. Either current_dividend or required rate is negative")
+    return (current_dividend * (growth_rate + 1)) / (req_rate - growth_rate)
+
+def __any_negative_value(values: list):
+    for val in values:
+        if val < 0:
+             return True
+    return False
