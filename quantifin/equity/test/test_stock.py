dd86f9edeafd6d09fd53ca7b65180b8aa1a416ee
Yue Da Chua <chua_yueda@hotmail.com>
2021-02-26 08:53:27 +0800

    abstract out getbeta mock

--- quantifin/equity/test/test_stock.py
+++ quantifin/equity/test/test_stock.py
@@ -7,29 +7,34 @@ from quantifin.equity import Stock
 from quantifin.equity.valuation import *
 from quantifin.util import markets
 
+@patch.object(yf.YahooFinancials, 'get_beta', return_value = 1.2)
 class TestCase(unittest.TestCase):
     def setUp(self):
         self.base_path = Path(__file__).parent
         self.index_json = self.read_json(str(self.base_path) + "/resource/market_price.json")
     
-
     def read_json(self, file_path):
         with open (file_path) as f:
             json_dict = load(f)
         return json_dict
 
-    @patch('yahoofinancials.YahooFinancials.get_beta')
     def test_initialise_stock(self, MockGetBeta):
-        MockGetBeta.return_value = 1.2
         apple = Stock("AAPL")
         self.assertEqual(yf.YahooFinancials.get_beta, MockGetBeta)
         self.assertTrue(MockGetBeta.called)
     
-    def test_CAPM(self):
+    def test_current_dividend(self, MockGetBeta):
+        apple= Stock("AAPL")
+
+        def get_current_dividend_side_effect():
+            dividend_path = str(self.base_path) + "/resource/market_price.json"
+            return self.read_json(dividend_path)
+    
+    def test_CAPM(self, MockGetBeta):
         req_rate_CAPM = CAPM(0.02, 0.1, 1.2)
         self.assertEqual(req_rate_CAPM, 0.116)
     
-    def test_gordon_growth_model(self):
+    def test_gordon_growth_model(self, MockGetBeta):
         current_dividend = 2.0 #NEEDS IMPLEMENTATION
         req_rate = CAPM(0.02, 0.10, 1.2)
         print(req_rate)
