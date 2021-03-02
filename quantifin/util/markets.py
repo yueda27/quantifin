c6884fb1dbb3c5859376cd2cd08cc0958b880d7b
Yue Da Chua <chua_yueda@hotmail.com>
2021-03-03 07:14:38 +0800

    refactor to have functions to be static functions of Market class

--- quantifin/util/markets.py
+++ quantifin/util/markets.py
@@ -15,6 +15,22 @@ class Market(yf.YahooFinancials):
     def __init__(self, market_code):
         self.market_code = EXCHANGES[market_code]
         super().__init__(self.market_code)
+    
+    @staticmethod
+    def get_market_price_list(index_json: dict):
+        index_code = list(index_json.keys())[0]
+        return index_json[index_code]['prices']
+
+    @staticmethod
+    def returns(market_price_list: list):
+        start_price = market_price_list[0]['close']
+        end_price = market_price_list[-1]['close']
+        market_return = (end_price / start_price)
+        return market_return
+
+    @staticmethod
+    def annualise(market_return: float, period: float):
+        return (market_return **(1/period)) - 1
 
     def get_annualised_return(self, year):
         def format_date(date: datetime):
@@ -25,20 +41,7 @@ class Market(yf.YahooFinancials):
             start_date = current_date - timedelta(weeks= 52 * year)
             return self.get_historical_price_data(format_date(start_date), format_date(current_date), 'monthly')
         price_resp = period_price_list(year)
-        return round(annualise(returns(get_market_price_list(price_resp)), year), 4)
+        return round(self.annualise(self.returns(self.get_market_price_list(price_resp)), year), 4)
         
 
 
-
-def get_market_price_list(index_json: dict):
-    index_code = list(index_json.keys())[0]
-    return index_json[index_code]['prices']
-
-def returns(market_price_list: list):
-    start_price = market_price_list[0]['close']
-    end_price = market_price_list[-1]['close']
-    market_return = (end_price / start_price)
-    return market_return
-
-def annualise(market_return: float, period: float):
-    return (market_return **(1/period)) - 1
--- quantifin/util/test/test_util.py
+++ quantifin/util/test/test_util.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import json
 from yahoofinancials import YahooFinancials
 
-from quantifin.util import markets
+from quantifin.util.markets import Market
 
 
 class TestCase(unittest.TestCase):
@@ -19,7 +19,7 @@ class TestCase(unittest.TestCase):
         return json_dict
 
     def test_get_market_price_list(self):
-        index_price_list = markets.get_market_price_list(self.index_json)
+        index_price_list = Market.get_market_price_list(self.index_json)
         index_code = list(self.index_json.keys())[0]
         self.assertEqual(index_price_list, self.index_json[index_code]['prices']) 
 
@@ -27,17 +27,17 @@ class TestCase(unittest.TestCase):
     def test_get_market_return(self):
         annual_return = 13597.9658203125  / 7729.31982421875
 
-        index_price_list = markets.get_market_price_list(self.index_json)
-        self.assertAlmostEqual(markets.returns(index_price_list), annual_return, 3)
+        index_price_list = Market.get_market_price_list(self.index_json)
+        self.assertAlmostEqual(Market.returns(index_price_list), annual_return, 3)
     
     def test_get_annualised_market_return(self):
-        market_returns = markets.returns(markets.get_market_price_list(self.index_json))
-        annualised_return = markets.annualise(market_returns, 2)
+        market_returns = Market.returns(Market.get_market_price_list(self.index_json))
+        annualised_return = Market.annualise(market_returns, 2)
         self.assertEqual(annualised_return, (((13597.9658203125  / 7729.31982421875) ** (1/2)) - 1))
     
     @patch.object(YahooFinancials, "get_historical_price_data")
     def test_Market_obj_init(self, MockGetPrice):
-        nasdaq = markets.Market("NasdaqGS")
+        nasdaq = Market("NasdaqGS")
         self.assertTrue(nasdaq.market_code, "^IXIC")
 
 
@@ -48,5 +48,5 @@ class TestCase(unittest.TestCase):
             return self.read_json(file_path)
 
         MockGetPrice.side_effect = get_price_side_effect
-        sp500 = markets.Market("S&P")
+        sp500 = Market("S&P")
         self.assertEqual(sp500.get_annualised_return(10), 0.1334)
