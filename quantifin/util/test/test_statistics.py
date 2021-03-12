3a545b07a08812e0e6e8437707521a1f4572345a
Yue Da Chua <chua_yueda@hotmail.com>
2021-03-12 20:15:05 +0800

    function for coeff variation and skew using scipy skew'

---
+++ quantifin/util/test/test_statistics.py
@@ -0,0 +1,20 @@
+import unittest
+from unittest.mock import patch
+from pathlib import Path
+import json
+import scipy.stats
+
+from quantifin.util.statistics import *
+
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        self.base_path = Path(__file__).parent
+        self.test_return = [10, 30, 20, 23]
+    
+    def test_coefficient_of_variation(self):
+        self.assertEqual(coefficient_of_variation(self.test_return),0.4001)
+    
+    def test_skew(self):
+        self.assertEqual(skew(self.test_return), -0.2959313655596756)
+        
+
