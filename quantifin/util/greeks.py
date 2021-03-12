d4aef936ec7480c554bd6cd6492b574d62b049c0
Yue Da Chua <chua_yueda@hotmail.com>
2021-03-12 16:34:27 +0800

    refactored sharpe ratio calculation

--- quantifin/util/greeks.py
+++ quantifin/util/greeks.py
@@ -9,30 +9,22 @@ def price_returns(price_list):
         returns.append((i[0] / i[1]) - 1)
     return returns
 
-def stdev_excess_return(price_returns, risk_free):
-    def excess_returns(re, rf):
-        return re - rf
-    def calc_variance(accum, excess):
-        return accum + (excess ** 2)
-    excess = [excess_returns(re, risk_free) for re in price_returns]
-    variance = reduce(calc_variance, excess,0)
-
-    return round(sqrt(variance / (len(price_returns) - 1)), 5)
-
-def sharpe_ratio_ex_post(price_list, risk_free):
-    '''Calculating after the fact sharpe ratio'''
-    re = price_returns(price_list)
-    average_return = sum(re) / len(re)
-    sigma = stdev_excess_return(re, risk_free)
-
-    return round((average_return - risk_free) / sigma, 3)
 
 def sharpe_ratio_ex_ante(expected_return, benchmark_rate, sigma):
     '''Predicting future sharpe ratio'''
     return round((expected_return - benchmark_rate) / sigma, 5)
-    
 
 
+def sharpe_ratio_ex_post(returns_list, benchmark):
+    differential_return = [__calculate_differential_return(re, benchmark) for re in returns_list]
+    sigma = stdev(differential_return)
+    return round(__sample_average(differential_return) / sigma, 5)
+
+def __calculate_differential_return(returns, benchmark):
+    return abs(returns - benchmark)
+
+def __sample_average(iterable):
+    return sum(iterable) / len(iterable)
 
 
 
--- quantifin/util/test/test_greeks.py
+++ quantifin/util/test/test_greeks.py
@@ -11,20 +11,23 @@ class TestCase(unittest.TestCase):
     def setUp(self):
         self.base_path = Path(__file__).parent
         self.price_list = read_price_list(str(self.base_path) + "/resource/price_list.json")[::-1]
+        self.sample_return = [-0.06, 0.2, -0.1, 0.24, -0.14, 0.2, 0.2, 0.16, 0.14]
     
     def test_price_return(self):
         price_list_count = len(self.price_list)
+        price_list = [100, 80, 70, 40]
+        result = [0.25, 0.1428571428571428, 0.75]
         correct_price_returns = read_price_list(str(self.base_path) + "/resource/price_return.json")
-
+        
+        self.assertEqual(price_returns(price_list), result)
         self.assertEqual(price_returns(self.price_list), correct_price_returns)
         self.assertEqual(len(price_returns(self.price_list)), price_list_count - 1)
-    
-    def test_stdev_excess_return(self):
-        price_return = [0.0818, 0.1047, 0.0247, 0.044, 0.2524, 0.0520, 0.2278, 0.28, 0.0387, 0.0598, 0.0183, 0.0586, 0.0051, 0.0541, 0.0617]
-        self.assertEqual(stdev_excess_return(price_return, 0.03), 0.10829)
-    
+
     def test_sharpe_ratio_ex_post(self):
-        self.assertEqual(sharpe_ratio_ex_post(self.price_list, 0.0001), 0.191)
+        returns = price_returns(self.price_list)
+        print("standard dev")
+        self.assertEqual(sharpe_ratio_ex_post(self.sample_return, 0.02), 3.6141)
+        self.assertEqual(sharpe_ratio_ex_post(returns, 0.005), 1.0652)
     
     def test_sharpe_ratio_ex_ante(self):
         self.assertEqual(sharpe_ratio_ex_ante(0.1, 0.03, 0.16), 0.43750)
