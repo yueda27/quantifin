6f41403b557a59c9afe1725c560cf77bb1cdbf79
Yue Da Chua <chua_yueda@hotmail.com>
2021-03-13 09:51:46 +0800

    updated sortino to accept benchmark lit

--- quantifin/util/test/test_greeks.py
+++ quantifin/util/test/test_greeks.py
@@ -12,6 +12,7 @@ class TestCase(unittest.TestCase):
         self.base_path = Path(__file__).parent
         self.price_list = read_price_list(str(self.base_path) + "/resource/price_list.json")[::-1]
         self.sample_return = [-0.06, 0.2, -0.1, 0.24, -0.14, 0.2, 0.2, 0.16, 0.14]
+        self.period_benchmark =  [0.02, 0.03, 0.026, 0.01, 0.022, 0.022, 0.01, 0.04, 0.01]
     
     def test_price_return(self):
         price_list_count = len(self.price_list)
@@ -24,10 +25,10 @@ class TestCase(unittest.TestCase):
         self.assertEqual(len(price_returns(self.price_list)), price_list_count - 1)
 
     def test_calculate_differential_return(self):
-        period_benchmark = [0.02, 0.03, 0.026, 0.01, 0.022, 0.022, 0.01, 0.04, 0.01]
-        self.assertRaises(ValueError, calculate_differential_returns, self.sample_return, period_benchmark[:-1])
+        self.assertRaises(ValueError, calculate_differential_returns, self.sample_return, self.period_benchmark[:-1])
         correct_result = [0.08, 0.17, 0.126, 0.23, 0.162, 0.178, 0.19, 0.12, 0.13]
-        self.assertEqual(calculate_differential_returns(self.sample_return, period_benchmark), correct_result)
+        self.assertEqual(calculate_differential_returns(self.sample_return, self.period_benchmark, absolute=True), correct_result)
+        self.assertEqual(calculate_differential_returns(self.sample_return, self.period_benchmark, absolute=False), [-0.08, 0.17, -0.126, 0.23, -0.162, 0.178, 0.19, 0.12, 0.13])
 
     def test_sharpe_ratio_ex_post(self):
         returns = price_returns(self.price_list)
@@ -40,8 +41,9 @@ class TestCase(unittest.TestCase):
 
     def test_sortino_ratio(self):
         return_rates = [0.17, 0.15, 0.23, -0.05, 0.12, 0.09, 0.13, -0.04]
-        self.assertEqual(sortino_ratio(return_rates, 0), 4.417)
-        self.assertEqual(sortino_ratio(return_rates, 0.03), 1.863)
+        self.assertRaises(ValueError, sortino_ratio, self.sample_return, self.period_benchmark[:-1])
+        self.assertEqual(sortino_ratio(return_rates, [0 for i in range(len(return_rates))]), 4.417)
+        # self.assertEqual(sortino_ratio(return_rates, [0.03 for range(len(return_rates))]), 1.863)
     
     def test_alpha(self):
         self.assertEqual(alpha(0.1, 0.08, 0.03, 1.2), 0.01) 
